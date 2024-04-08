0b83e37a1a411431cf03bd70bc288de43e5a8d9a
bamcerr <bamcerr@gmail.com>
2024-04-08 16:16:44 +0900

    Themes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
 import styled, { keyframes } from 'styled-components'
 
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-
-  &:active {
-    opacity: 0;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-
-  ${Emoji}:hover {
-    font-size: 98px;
-  }
-`;
-
 
 function App() {
   return <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
-      <Emoji>🔥</Emoji>
+    <Title>Hello</Title>
   </Wrapper>
 }
 
