e13763b22915a802055198cb356c27e7d4464ac7
bamcerr <bamcerr@gmail.com>
2024-04-10 08:54:59 +0900

    Home part Two

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
 const Container = styled.div`
   padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -65,21 +68,48 @@ const coins = [
     is_active: true,
     type: "token",
   },
-]
+];
+
+const Loader = styled.span`
+  text-align: center;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: String;
+  symbol: String;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+};
 
 function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  
+  useEffect(() => {
+    (async() => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json()
+      setCoins(json.slice(0, 100))
+      setLoading(false)
+    })()
+  }, [])
+
   return <Container>
     <Header>
       <Title>코인</Title>
     </Header>
 
-    <CoinList>
+    {loading ? <Loader>loading...</Loader> : <CoinList>
       {coins.map(coin => (
         <Coin key={coin.id}>
           <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
         </Coin>
       ))}
-    </CoinList>
+    </CoinList>}
   </Container>
 }
 
