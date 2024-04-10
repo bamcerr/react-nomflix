dc1b8be8e92c5ea474e51648c34fdcc70116e2d0
bamcerr <bamcerr@gmail.com>
2024-04-10 10:12:57 +0900

    Nested Routes part One

--- src/App.tsx
+++ src/App.tsx
@@ -56,6 +56,8 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Source Sans Pro', sans-serif;
     background-color: ${props => props.theme.bgColor};
     color: ${props => props.theme.textColor};
+    font-weight: 300;
+    line-height: 1.2;
   }
 
   a {
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,60 @@
 import { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom"
+import { Route, Switch, useLocation, useParams } from "react-router-dom"
 import styled from "styled-components";
+import Price from "./Price";
+import Chart from "./Chart";
+
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${props => props.theme.accentColor};
+`;
+
+const Overview = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  span:first-child {
+    font-size: 10px;
+    font-weight: 400;
+    text-transform: uppercase;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  margin: 20px 0px;
+`;
+
 
 // mark: - info data interface
-export interface InfoData {
+interface InfoData {
   id:                 string;
   name:               string;
   symbol:             string;
@@ -30,7 +81,7 @@ export interface InfoData {
   last_data_at:       Date;
 }
 
-export interface Links {
+interface Links {
   explorer:    string[];
   facebook:    string[];
   reddit:      string[];
@@ -39,39 +90,39 @@ export interface Links {
   youtube:     string[];
 }
 
-export interface LinksExtended {
+interface LinksExtended {
   url:    string;
   type:   string;
   stats?: Stats;
 }
 
-export interface Stats {
+interface Stats {
   subscribers?:  number;
   contributors?: number;
   stars?:        number;
   followers?:    number;
 }
 
-export interface Tag {
+interface Tag {
   id:           string;
   name:         string;
   coin_counter: number;
   ico_counter:  number;
 }
 
-export interface Team {
+interface Team {
   id:       string;
   name:     string;
   position: string;
 }
 
-export interface Whitepaper {
+interface Whitepaper {
   link:      string;
   thumbnail: string;
 }
 
 // mark: - price data interface
-export interface PriceData {
+interface PriceData {
   id:            string;
   name:          string;
   symbol:        string;
@@ -84,11 +135,11 @@ export interface PriceData {
   quotes:        Quotes;
 }
 
-export interface Quotes {
+interface Quotes {
   USD: Usd;
 }
 
-export interface Usd {
+interface Usd {
   price:                  number;
   volume_24h:             number;
   volume_24h_change_24h:  number;
@@ -108,43 +159,20 @@ export interface Usd {
   percent_from_price_ath: number;
 }
 
-
-
+// mark: 
 interface RouteParams {
   coinId: string;
 }
-
-const Container = styled.div`
-  padding: 0px 20px;
-  max-width: 480px;
-  margin: 0 auto;
-`;
-
-const Header = styled.header`
-  height: 15vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Loader = styled.span`
-  text-align: center;
-  display: block;
-`;
-
-const Title = styled.h1`
-  font-size: 48px;
-  color: ${props => props.theme.accentColor};
-`;
 
 interface RouteState {
   name: string;
 }
 
+
 function Coin() {
   const { coinId } = useParams<RouteParams>();
-  const [loading, setLoading] = useState(true);
   const {state} = useLocation<RouteState>();
+  const [loading, setLoading] = useState(true);
   const [info, setInfo] = useState<InfoData>();
   const [priceInfo, setPriceInfo] = useState<PriceData>();
 
@@ -160,15 +188,53 @@ function Coin() {
 
       setInfo(infoData);
       setPriceInfo(priceData);
+      setLoading(false);
     })()
-  }, [])
+  }, [coinId])
 
   return <Container>
   <Header>
-    <Title>{state?.name || "Loading.."}</Title>
+    <Title>{state?.name ? state.name : loading ? "Loading.." : info?.name}</Title>
   </Header>
 
-  {loading ? <Loader>Loading...</Loader> : null}
+  {loading 
+    ? <Loader>Loading...</Loader> 
+    : <>
+        <Overview>
+          <OverviewItem>
+            <span>Rank:</span>
+            <span>{info?.rank}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Symbol:</span>
+            <span>{info?.symbol}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Open Source:</span>
+            <span>{info?.open_source ? "Yes" : "No"}</span>
+          </OverviewItem>
+        </Overview>
+        <Description>{info?.description}</Description>
+        <Overview>
+          <OverviewItem>
+            <span>Total Suply:</span>
+            <span>{priceInfo?.total_supply}</span>
+          </OverviewItem>
+          <OverviewItem>
+            <span>Max Supply:</span>
+            <span>{priceInfo?.max_supply}</span>
+          </OverviewItem>
+        </Overview>
+        <Switch>
+          <Route path={`/${coinId}/price`}>
+            <Price />
+          </Route>
+          <Route path={`/${coinId}/chart`}>
+            <Chart />
+          </Route>
+        </Switch>
+      </>
+  }
   </Container>
 }
 
