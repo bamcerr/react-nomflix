490ef97089ee16f281c049765d2490afd5ba292f
bamcerr <bamcerr@gmail.com>
2024-04-18 07:17:32 +0900

    Get Selectors

--- src/atoms.ts
+++ src/atoms.ts
@@ -1,56 +1,14 @@
 import { atom, selector } from "recoil";
-import { recoilPersist } from "recoil-persist";
 
-const {persistAtom} = recoilPersist();
-
-export enum Categories {
-  "TO_DO",
-  "DOING",
-  "DONE"
-}
-
-export type TCategories = Categories | string;
-
-export interface ICategory {
-  displayName: string;
-  name: TCategories;
-  id: string;
-}
-
-export interface IToDo {
-  text: string;
-  id: number;
-  category: ICategory;
-}
-
-export const categoriesState = atom<ICategory[]>({
-  key: 'categories',
-  default: [
-    {name: Categories[0], id: String(Categories.TO_DO), displayName: "To do"},
-    {name: Categories[1], id: String(Categories.DOING), displayName: "Doing"},
-    {name: Categories[2], id: String(Categories.DONE), displayName: "Done"},
-  ],
-  effects_UNSTABLE: [persistAtom],
+export const minuteState = atom({
+  key: 'minutes',
+  default: 0
 });
 
-export const categoryState = atom<ICategory>({
-  key: 'category',
-  default: {name: Categories[0], id: String(Categories.TO_DO), displayName: "To do"},
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-  effects_UNSTABLE: [persistAtom],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-
-    return toDos.filter(toDo => toDo.category.id === category.id);
+    const minutes = get(minuteState)
+    return minutes / 60
   }
 });
