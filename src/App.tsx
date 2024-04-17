892df6103be95a2789f2fc71f8526b5064cefaa2
bamcerr <bamcerr@gmail.com>
2024-04-18 08:53:20 +0900

    Performance

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useRecoilState } from "recoil";
 
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import DraggableCard from "./components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
@@ -28,13 +29,6 @@ const Board = styled.div`
   min-height: 200px;
 `;
 
-const Card = styled.div`
-  border-radius: 5px;
-  margin-bottom: 5px;
-  padding: 10px 10px;
-  background-color: ${(props) => props.theme.cardColor};
-`;
-
 const toDos = ["a", "b", "c", "d", "e", "f"];
 
 function App() {
@@ -44,16 +38,8 @@ function App() {
 
     setToDos((oldToDos) => {
       const toDosCopy = [...oldToDos];
-      // 1) Delete item on source.index
-      console.log("Delete item on", source.index);
-      console.log(toDosCopy);
       toDosCopy.splice(source.index, 1);
-      console.log("Deleted item");
-      console.log(toDosCopy);
-      // 2) Put back the item on the destination.index
-      console.log("Put back", draggableId, "on ", destination.index);
       toDosCopy.splice(destination?.index, 0, draggableId);
-      console.log(toDosCopy);
       return toDosCopy;
     });
   }
@@ -65,13 +51,7 @@ function App() {
         {(magic) => (
           <Board ref={magic.innerRef} {...magic.droppableProps}>
             {toDos.map((toDo, index) => (
-              <Draggable key={toDo} draggableId={toDo} index={index}>
-                {(magic) => (
-                  <Card ref={magic.innerRef} {...magic.dragHandleProps} {...magic.draggableProps}>
-                    {toDo}
-                  </Card>
-                )}
-              </Draggable>
+              <DraggableCard key={toDo} toDo={toDo} index={index} />
             ))}
             {magic.placeholder}
           </Board>
