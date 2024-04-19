eac9d6f27354f6d2ff6e2b82cf65ac27558a9cb0
bamcerr <bamcerr@gmail.com>
2024-04-19 17:18:31 +0900

    Variants part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion } from "framer-motion"
+import { Variants, motion } from "framer-motion"
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -17,13 +17,14 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVar = {
+  start: {scale: 0},
+  end: {scale: 1, rotateZ: 360, transition: {type: 'spring', delay: 0.5}}
+}
+
 function App() {
   return (<Wrapper>
-    <Box 
-      transition={{type: "spring", delay: 0.5}}
-      initial={{scale: 0}}
-      animate={{scale: 1, rotateZ: 360}}
-    />
+    <Box variants={myVar} initial={`start`} animate={`end`}/>
   </Wrapper>)
 }
 
