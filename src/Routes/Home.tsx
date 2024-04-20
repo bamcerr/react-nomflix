85cbd5762029e38eddff954b125cddaa624b1aa5
bamcerr <bamcerr@gmail.com>
2024-04-20 16:11:02 +0900

    Slider part One

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query"
 import { IGetMoviesResult, getMovies } from "../api"
 import { styled } from "styled-components";
 import { makeImagePath } from "../utils";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 
 const Wrapper = styled.div`
@@ -15,13 +17,13 @@ const Loader = styled.div`
   align-items: center;
 `;
 
-const Banner = styled.div<{bgPhoto: string}>`
+const Banner = styled.div<{$bgPhoto: string}>`
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
-  background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)), url(${(props) => props.bgPhoto});
+  background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,1)), url(${(props) => props.$bgPhoto});
   background-size: cover;
 `;
 
@@ -35,8 +37,43 @@ const Overview = styled.p`
   width: 50%;
 `;
 
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  position: absolute;
+  width: 100%;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 66px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.outerHeight + 10
+  },
+  visible: {
+    x: 0
+  },
+  exit: {
+    x: -window.outerHeight - 10
+  }
+}
+
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>({queryKey:["movies", "nowPlaying"], queryFn:getMovies});
+
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex(prev => prev + 1);
 
   return(
     <Wrapper>
@@ -44,12 +81,26 @@ function Home() {
         ? <Loader>Loading...</Loader> 
         : <>
           <Banner
-            onClick={() => {}}
-            bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')} 
+            onClick={increaseIndex}
+            $bgPhoto={makeImagePath(data?.results[0].backdrop_path || '')} 
           >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit={'exit'}
+                transition={{type: "tween", duration: 1}}
+                key={index}
+              >
+                {[1,2,3,4,5,6].map((i) => <Box key={i}>{i}</Box>)}
+              </Row>
+            </AnimatePresence>
+          </Slider>
         </>
       }
     </Wrapper>
