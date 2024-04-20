f998381dc8e9e6bb8b7ccc8341724f43ee5711aa
bamcerr <bamcerr@gmail.com>
2024-04-20 18:25:49 +0900

    Search Redirect

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,8 @@
-import { Link, useRouteMatch } from "react-router-dom"
+import { Link, useHistory, useRouteMatch } from "react-router-dom"
 import { styled } from "styled-components"
 import { motion, useAnimation, useMotionValueEvent, useScroll } from "framer-motion"
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Nav = styled(motion.nav)`
   display: flex;
@@ -51,7 +52,7 @@ const Item = styled.li`
 `;
 
 
-const Search = styled.span`
+const Search = styled.form`
   color: white;
   display: flex;
   align-items: center;
@@ -108,6 +109,9 @@ const navVariants = {
   }
 }
 
+interface IForm {
+  keyword: string;
+}
 
 function Header() {
   const [searchOpen, setSearchOpen] = useState(false);
@@ -116,6 +120,12 @@ function Header() {
   const inputAnimation = useAnimation();
   const navAnimation = useAnimation();
   const { scrollY } = useScroll();
+
+  const history = useHistory();
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data: IForm) => {
+    history.push(`/search?keyword=${data.keyword}`);
+  }
 
 
   const toggleSearch = () => {
@@ -159,7 +169,7 @@ function Header() {
         </Items>
       </Col>
       <Col>
-        <Search>
+        <Search onSubmit={handleSubmit(onValid)}>
           <motion.svg
             onClick={toggleSearch}
             animate={{x: searchOpen ? -185 : 0}}
@@ -175,6 +185,7 @@ function Header() {
             ></path>
           </motion.svg>
           <Input 
+            {...register('keyword', {required: true, minLength: 2})}
             animate={inputAnimation}
             initial={{scaleX: 0}}
             transition={{type: "linear"}}
