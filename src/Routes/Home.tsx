3b320a69819526b0e8952def2135526dba75546a
bamcerr <bamcerr@gmail.com>
2024-04-20 18:13:21 +0900

    Movie Modal part Three

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -98,6 +98,31 @@ const BigMovie = styled(motion.div)`
   left: 0;
   right: 0;
   margin: 0 auto;
+  border-radius: 15px;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.black.lighter};
+`;
+
+const BigCover = styled.div`
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+  height: 400px;
+`;
+
+const BigTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  font-size: 46px;
+  position: relative;
+  top: -80px;
+`;
+
+const BigOverview = styled.p`
+  padding: 20px;
+  position: relative;
+  top: -80px;
+  color: ${(props) => props.theme.white.lighter};
 `;
 
 const rowVariants = {
@@ -165,6 +190,8 @@ function Home() {
 
   const onOverlayClick = () => history.push('/');
 
+  const clickedMovie = bigMovieMatch?.params.movieId && data?.results.find((movie) => movie.id === +bigMovieMatch.params.movieId);
+
   const movies = data?.results.slice(1).slice(offset * index, offset * index + offset);
   return(
     <Wrapper>
@@ -220,7 +247,18 @@ function Home() {
               <BigMovie
                 style={{top: scrollY.get() + 100}}
                 layoutId={bigMovieMatch.params.movieId}
-              >hello</BigMovie>
+              >
+                {clickedMovie && <>
+                  <BigCover 
+                    style={{
+                      backgroundImage: `linear-gradient(to top, black, transparent), url(${makeImagePath(clickedMovie.backdrop_path, "w500")})`
+                    }}
+                  />
+                  <BigTitle>{clickedMovie.title}</BigTitle>
+                  <BigOverview>{clickedMovie.overview}</BigOverview>
+                </>}
+
+              </BigMovie>
             </> : null}
           </AnimatePresence>
         </>
